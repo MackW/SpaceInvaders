dd80f3897f5e5d506ef144c3271e07a5a29d39d7
Mark Wharton <github@mack.je>
2014-02-22 20:09:34 +0000

    General

--- Space Invaders/Code/Main.py
+++ Space Invaders/Code/Main.py
@@ -14,14 +14,15 @@ if not pygame.mixer: print 'Warning, sound disabled'
 
 class SInvaders:
 
-    invaderMap =[[0,0,0,0,0,0,0,0,0],
-                 [0,0,0,0,0,0,0,0,0],
-                 [1,1,1,1,1,1,1,1,1],
-                 [1,1,1,1,1,1,1,1,1],
-                 [2,2,2,2,2,2,2,2,2],
-                 [2,2,2,2,2,2,2,2,2]]
+    invaderMap =[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0],
+                 [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0],
+                 [2,2,2,2,2,2,2,2,0,0,0,0,0,0,0],
+                 [2,2,2,2,2,2,2,2,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
     currentFrame=0
-
+    sprInvaders = []
     def __init__(self, width=1024,height=768):
         pygame.init()
         self.width = width
@@ -51,21 +52,35 @@ class SInvaders:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                     sys.exit()
+            for b in self.sprInvaders:
+                self.screen.fill((0,0,0), b.rect)
+                b.move()
+                self.screen.blit(b.image, b.rect)
             
-            self.Invaders_sprites.clear(self.screen,self.background)
+            
+            """self.Invaders_sprites.clear(self.screen,self.background)
             self.Invaders.move()
-            self.Invaders_sprites.draw(self.screen)
+            self.Invaders_sprites.draw(self.screen)"""
             pygame.display.flip()
             """ elif event.type == KEYDOWN:
                invaderMap=invaderMap"""
             clock.tick(10)
+    
+    def MoveInvaderMap(self):
+        # stub method
+        b=1
                         
     def LoadSprites(self):
         """Load the sprites that we need"""
-        self.Invaders = Invader()
-        self.Invaders_sprites = pygame.sprite.RenderPlain((self.Invaders))
-        self.Invaders = Invader()
-        self.Invaders_sprites = pygame.sprite.RenderPlain((self.Invaders))
+        for iLCa in xrange(0, len(self.invaderMap[1])):
+            if self.invaderMap[1][iLCa]==1:
+                sInv = Invader()
+                sInv.rect.x=iLCa*120
+                sInv.rect.y=50
+                self.sprInvaders.append(sInv)
+            
+
+
  
 class Invader(pygame.sprite.Sprite):
 
@@ -76,10 +91,17 @@ class Invader(pygame.sprite.Sprite):
         self.images.append(load_image('Row3Closed.png',-1))
         self.image = self.images[0]
         self.rect=self.image.get_rect()
-        self.xdirection =0
+        self.xdirection =1
         self.currentFrame = 0
+        self.frameCountToMove = 5
+        self.tick=0
         
     def move(self):
+        if self.frameCountToMove>0:
+            self.frameCountToMove=self.frameCountToMove-1
+            return
+        else:
+            self.frameCountToMove = 5
         self.currentFrame=0 if self.currentFrame==1 else 1
         self.image = self.images[self.currentFrame]
         xMove = 10;
